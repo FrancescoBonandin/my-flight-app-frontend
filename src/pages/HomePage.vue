06447aeab3645432fca7000cd4b2b93bdc7fee96
FrancescoBonandin <bonandinfrancesco@gmail.com>
2024-01-09 18:21:47 +0100

    data visualization fix

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -13,11 +13,26 @@
                 airports:null,
                 departureAirport:null,
                 arrivalAirport:null,
-
+                departureDate:null,
+                results:null
             }
 
         },
         methods:{
+
+            isParamsOk(){
+
+                if((this.departureAirport=='departure airport'||this.departureAirport==null)
+                || (this.arrivalAirport=='arrival airport'||this.arrivalAirport==null)) {
+                    console.log(this.departureAirport, '|', this.arrivalAirport)
+                    return true
+                }
+
+                else{
+                    console.log(this.departureAirport, '|', this.arrivalAirport)
+                    return false
+                }
+            },
 
             getAirports(){
 
@@ -32,8 +47,30 @@
                 .catch(error=>{
                     console.log(error)
                 })
-            }
-        },
+            },
+
+            filter(){
+
+                this.results=null
+
+                axios.get('http://127.0.0.1:8000/api/flights', {
+                    params:{
+                        'departure-airport' : this.departureAirport,
+                        'arrival-airport' : this.arrivalAirport,
+                        'departure-date' : this.departureDate
+                    }
+                    }).then(response=>{
+                        this.results=response.data.flights
+                        console.log(this.results)
+                    })
+                    .catch(error=>{
+                        console.log(error)
+                    })
+                }
+            },
+            computed:{
+                
+            },
         created(){
             this.getAirports()
         }
@@ -65,15 +102,14 @@
                 
             </div>
             
-            <form action="" method="get">
+            <form @submit.prevent="filter()" method="get">
 
                 <div>
                     <label for="departure-airport"> Departure Airport:</label>
                     <select name="departure-airport" id="departure-airport" v-model="departureAirport" required>
 
-
-                        <option value="null" disabled selected > departure airport</option>
-                        <option v-for="(airport, key) in airports" :key="key" :value="airport" :disabled="airport===arrivalAirport"> {{ airport }}</option>
+                        <option value="null" disabled selected >departure airport</option>
+                        <option v-for="(airport, key) in airports" :key="key" :value="airport" :disabled="airport===arrivalAirport"> {{ airport }} </option>
 
                     </select>
                 </div>
@@ -82,8 +118,8 @@
                     <label for="arrival-airport"> Arrival Airport:</label>
                     <select name="arrival-airport" id="arrival-airport" v-model="arrivalAirport" required>
 
-                        <option value="null" disabled selected> arrival airport</option>
-                        <option v-for="(airport, key) in airports" :key="key" :value="airport" :disabled="airport===departureAirport" > {{ airport }}</option>
+                        <option value="null" disabled selected>arrival airport</option>
+                        <option v-for="(airport, key) in airports" :key="key" :value="airport" :disabled="airport===departureAirport" > {{ airport }} </option>
 
                     </select>
                 </div>
@@ -92,16 +128,59 @@
 
                     <label for="departure-date">Departure Date</label>
 
-                    <input type="date" name="departure-date" id="departure-date" value="2013-05-21">
+                    <input type="date" name="departure-date" id="departure-date" value="2013-05-21" :v-model="departureDate">
 
                 </div>
 
                 <div>
-                    <input type="submit" value="search">
+                    <input type="submit" value="search" :disabled="isParamsOk()">
                 </div>
             </form>
+            
+            <div class="row">
+    
+                <div v-if="results==null || results.length<=0" class="col-12 text-center" >
+                    no available flight
+                </div>
+    
+                <div v-for="(flight, key) in results" class="col-12">
+    
+                    <div class="card">
+
+                        <div class="col">
+                            
+                            {{ flight.number }}
+
+                        </div>
+
+                        <div class="col">
+                            
+                            {{ flight.departure_datetime }}
+
+                        </div>  
+
+                        <div class="col">
+                            
+                            {{ flight.arrival_datetime }}
+
+                        </div>
+
+                        <div class="col">
+                            {{ flight.departure_airport.name}}
+                        </div>
+
+                        <div class="col">
+                            {{ flight.arrival_airport.name }}
+                        </div>
+
+                    </div>
+    
+                </div>
+
+            </div>
 
         </div>
+
 
     </main>
 
